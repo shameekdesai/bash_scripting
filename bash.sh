725e9295a4cc02005de3261eef5f6c0a7297ed1b
shameekdesai <desaishameek@gmail.com>
2021-06-01 18:20:06 +0530

    if else condition

--- bash.sh
+++ bash.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/bash
 count=10  #no space for equal to operator 
 
-if [ $count -eq 10 ] # space at the brackets 
+if [ $count -eq 9 ] # space at the brackets 
 then
 	echo " count is 10"
+else
+	echo " the condition is false "
 fi
