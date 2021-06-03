7998f9465a38418555a244d6c81d596d44ab4133
shameekdesai <desaishameek@gmail.com>
2021-06-03 14:56:44 +0530

    arithmetic

--- bash.sh
+++ bash.sh
@@ -1,9 +1,4 @@
 #! /usr/bin/bash
-read VAR1;
-if [[ $VAR1 == y ]] || [[ $VAR1 == Y ]]
-then
-  echo "YES"
-elif [[ $VAR1 == n ]] || [[ $VAR1 == N ]]
-then
-	echo "NO"
-fi;
+read string;
+out=`echo "scale=4;$string"|bc -l`;
+printf "%0.3f\n" $out;
